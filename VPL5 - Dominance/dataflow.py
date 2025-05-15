19fe3509d383fe4e09480dc37854f327be5cd49c
Mathias Oliveira <mtgo007@gmail.com>
2025-05-15 18:14:09 -0300

    VPL4

---
+++ VPL5 - Dominance/dataflow.py
@@ -0,0 +1,259 @@
+from lang import *
+from abc import ABC, abstractmethod
+
+
+class DataFlowEq(ABC):
+    """
+    A class that implements a data-flow equation. The key trait of a data-flow
+    equation is an `eval` method, which evaluates that equation. The evaluation
+    of an equation might change the environment that associates data-flow facts
+    with identifiers.
+    """
+
+    def __init__(self, instruction):
+        """
+        Every data-flow equation is produced out of a program instruction. The
+        initialization of the data-flow equation verifies if, indeed, the input
+        object is an instruction.
+        """
+        assert isinstance(instruction, Inst)
+        self.inst = instruction
+
+    @classmethod
+    @abstractmethod
+    def name(self) -> str:
+        """
+        The name of a data-flow equation is used to retrieve the data-flow
+        facts associated with that equation in the environment. For instance,
+        imagine that we have an equation like this one below:
+
+        "OUT[p] = (v, p) + (IN[p] - (v, _))"
+
+        This equation affects OUT[p]. We store OUT[p] in a dictionary. The name
+        of the equation is used as the key in this dictionary. For instance,
+        the name of the equation could be 'OUT_p'.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def eval_aux(self, data_flow_env) -> set:
+        """
+        This method determines how each concrete equation evaluates itself.
+        In a way, this design implements the 'template method' pattern. In other
+        words, the DataFlowEq class implements a concrete method eval, which
+        calls the abstract method eval_aux. It is the concrete implementation of
+        eval_aux that determines how the environment is affected by the
+        evaluation of a given equation.
+        """
+        raise NotImplementedError
+
+    def eval(self, data_flow_env) -> bool:
+        """
+        This method implements the abstract evaluation of a data-flow equation.
+        Notice that the actual semantics of this evaluation will be implemented
+        by the `èval_aux` method, which is abstract.
+        """
+        old_env = data_flow_env[self.name()]
+        data_flow_env[self.name()] = self.eval_aux(data_flow_env)
+        return True if data_flow_env[self.name()] != old_env else False
+
+
+class Dominance_Eq(DataFlowEq):
+    """
+    This concrete class implements the meet operation for the dominance
+    analysis. The dominators of data-flow vertex v is the intersection of the
+    dominators of the predecessors of v.
+    """
+
+    def eval_aux(self, env):
+        """
+        The evaluation of the meet operation for the dominance relation.
+        Basically: D[n] = {n} U Intersection(D[p], for p in n.preds)
+
+        Example:
+            >>> Inst.next_index = 0
+            >>> i0 = Add('x', 'a', 'b')
+            >>> df = Dominance_Eq(i0)
+            >>> sorted(df.eval_aux({}))
+            [0]
+
+            >>> Inst.next_index = 0
+            >>> i0 = Add('x', 'a', 'b')
+            >>> i1 = Add('y', 'x', 'a')
+            >>> i2 = Add('y', 'x', 'b')
+            >>> i3 = Add('z', 'x', 'y')
+            >>> i0.add_next(i1)
+            >>> i0.add_next(i2)
+            >>> i1.add_next(i3)
+            >>> i2.add_next(i3)
+            >>> df = Dominance_Eq(i3)
+            >>> sorted(df.eval_aux({'1': {0, 1}, '2': {0, 2}}))
+            [0, 3]
+        """
+        # TODO: Implement this method.
+        if(len(self.inst.preds) > 0):
+            sol = UniversalSet()
+            for p in self.inst.preds:
+                sol = sol & env[str(p.ID)]
+
+            if isinstance(sol, UniversalSet):
+                return sol
+            else:
+                return sol | set([self.inst.ID])
+        else:
+            return set([self.inst.ID])
+
+
+    def name(self):
+        """
+        The name of this dataflow equation. Ex.:
+            >>> Inst.next_index = 0
+            >>> add = Add('x', 'a', 'b')
+            >>> eq = Dominance_Eq(add)
+            >>> eq.name()
+            '0'
+        """
+        return str(self.inst.ID)
+
+
+    def __str__(self):
+        """
+        The name of a dominance equation is always in the format:
+        D(n) = set(n) U Intersection( D(p1), D(p2), ... D(pn) )
+        where each pi is a predecessor of n.
+
+        Example:
+            >>> Inst.next_index = 0
+            >>> i0 = Add('w', 'a', 'b')
+            >>> i1 = Add('x', 'a', 'a')
+            >>> i2 = Add('y', 'b', 'b')
+            >>> i3 = Add('z', 'x', 'y')
+            >>> i0.add_next(i3)
+            >>> i1.add_next(i3)
+            >>> i2.add_next(i3)
+            >>> df = Dominance_Eq(i3)
+            >>> str(df)
+            'D(3) = set(3) U Intersection( D(0), D(1), D(2) )'
+        """
+        ps = ", ".join(sorted([f"D({pred.ID})" for pred in self.inst.preds]))
+        return f"D({self.name()}) = set({self.inst.ID}) U Intersection( {ps} )"
+
+
+def dominance_constraint_gen(insts):
+    """
+    Builds a list of equations to solve Dominance Analysis for the given set of
+    instructions.
+
+    Example:
+        >>> Inst.next_index = 0
+        >>> i0 = Add('w', 'a', 'b')
+        >>> i1 = Add('x', 'a', 'a')
+        >>> i2 = Add('y', 'b', 'b')
+        >>> i3 = Add('z', 'x', 'y')
+        >>> i0.add_next(i3)
+        >>> i1.add_next(i3)
+        >>> i2.add_next(i3)
+        >>> insts = [i0, i1, i2, i3]
+        >>> sol = [str(eq) for eq in dominance_constraint_gen(insts)]
+        >>> sol[3]
+        'D(3) = set(3) U Intersection( D(0), D(1), D(2) )'
+    """
+    # TODO: Implement this function.
+    return [Dominance_Eq(inst) for inst in insts]
+
+
+class UniversalSet(set):
+    """
+    This class implements a 'set of everything'. Basically, the intersection of
+    a universal set with any other set is always the other set. We use this
+    set of everything to simulate the TOP of a JOIN semi-lattice. The TOP is
+    the element that, once joined with any other element, will be the other
+    element. We initialize the set of dominators with the TOP, for instance.
+    """
+    def __and__(self, other):
+        """
+        The AND operation of a universal set and any set is always the other
+        set. Ex.:
+            >>> sorted(set([1, 2, 3]) & UniversalSet())
+            [1, 2, 3]
+            >>> set() & UniversalSet()
+            set()
+        """
+        return other
+
+    def __rand__(self, other):
+        """
+        The AND operation of any set and a universal set is always the other
+        set. Ex.:
+            >>> UniversalSet() & set()
+            set()
+            >>> sorted(UniversalSet() & set(['a', 'b']))
+            ['a', 'b']
+        """
+        return other
+
+
+def abstract_interp(equations):
+    """
+    This function iterates on the equations, solving them in the order in which
+    they appear. It returns an environment with the solution to the data-flow
+    analysis.
+
+    Example for liveness analysis:
+        >>> Inst.next_index = 0
+        >>> i0 = Add('w', 'a', 'b')
+        >>> i1 = Add('x', 'a', 'a')
+        >>> i2 = Add('y', 'b', 'b')
+        >>> i3 = Add('z', 'x', 'y')
+        >>> i0.add_next(i3)
+        >>> i1.add_next(i3)
+        >>> i2.add_next(i3)
+        >>> insts = [i0, i1, i2, i3]
+        >>> eqs = dominance_constraint_gen(insts)
+        >>> sol = abstract_interp(eqs)
+        >>> f"D(0): {sorted(sol['0'])}, D(1): {sorted(sol['3'])}"
+        'D(0): [0], D(1): [3]'
+
+        >>> Inst.next_index = 0
+        >>> i0 = Add('w', 'a', 'b')
+        >>> i1 = Add('x', 'a', 'a')
+        >>> i2 = Add('y', 'b', 'b')
+        >>> i3 = Add('z', 'x', 'y')
+        >>> i0.add_next(i3)
+        >>> i1.add_next(i3)
+        >>> i2.add_next(i3)
+        >>> i0.add_next(i1)
+        >>> i0.add_next(i2)
+        >>> insts = [i0, i1, i2, i3]
+        >>> eqs = dominance_constraint_gen(insts)
+        >>> sol = abstract_interp(eqs)
+        >>> f"D(0): {sorted(sol['0'])}, D(1): {sorted(sol['3'])}"
+        'D(0): [0], D(1): [0, 3]'
+
+        >>> Inst.next_index = 0
+        >>> c0 = Add('c', 'zero', 'zero')
+        >>> repeat = Lth('repeat', 'c', 'N')
+        >>> bt = Bt('repeat')
+        >>> c1 = Add('c', 'c', 'one')
+        >>> answer = Add('answer', 'c', 'zero')
+        >>> c0.add_next(repeat)
+        >>> repeat.add_next(bt)
+        >>> bt.add_next(answer)
+        >>> bt.add_true_next(c1)
+        >>> c1.add_next(repeat)
+        >>> insts = [c0, repeat, bt, c1, answer]
+        >>> eqs = dominance_constraint_gen(insts)
+        >>> s = abstract_interp(eqs)
+        >>> f"D(0): {sorted(s['0'])}, D(1): {sorted(s['1'])}"
+        'D(0): [0], D(1): [0, 1]'
+    """
+    # TODO: Implement this function.
+    env = {eq.name(): UniversalSet() for eq in equations}
+
+    changed = True
+    while(changed):
+        changed = False
+        for eq in equations:
+            changed = changed | eq.eval(env)
+    return env
---
+++ VPL5 - Dominance/parser.py
@@ -0,0 +1,103 @@
+"""
+This file implements a parser: a function that reads a text file, and returns
+a control-flow graph of instructions plus an environment mapping variables to
+integer values. The text file has the following format:
+
+    [First line] A dictionary describing the environment
+    [n-th line] The n-th instruction in our program.
+
+As an example, the program below sums up the numbers a, b and c:
+
+    {"a": 1, "b": 3, "c": 5}
+    x = add a b
+    l2 = x = add x c
+"""
+
+from lang import *
+
+def line2env(line):
+    """
+    Maps a string (the line) to a dictionary in python. This function will be
+    useful to read the first line of the text file. This line contains the
+    initial environment of the program that will be created. If you don't like
+    the function, feel free to drop it off.
+
+    Example
+        >>> line2env('{"zero": 0, "one": 1, "three": 3, "iter": 9}').get('one')
+        1
+    """
+    import json
+    env_dict = json.loads(line)
+    env_lang = Env()
+    for k, v in env_dict.items():
+        env_lang.set(k, v)
+    return env_lang
+
+def file2cfg_and_env(lines):
+    """
+    Builds a control-flow graph representation for the strings stored in
+    `lines`. The first string represents the environment. The other strings
+    represent instructions.
+
+    Example:
+        >>> l0 = '{"a": 0, "b": 3}'
+        >>> l1 = 'bt a 1'
+        >>> l2 = 'x = add a b'
+        >>> env, prog = file2cfg_and_env([l0, l1, l2])
+        >>> interp(prog[0], env).get("x")
+        3
+
+        >>> l0 = '{"a": 1, "b": 3, "x": 42, "z": 0}'
+        >>> l1 = 'bt a 2'
+        >>> l2 = 'x = add a b'
+        >>> l3 = 'x = add x z'
+        >>> env, prog = file2cfg_and_env([l0, l1, l2, l3])
+        >>> interp(prog[0], env).get("x")
+        42
+
+        >>> l0 = '{"a": 1, "b": 3, "c": 5}'
+        >>> l1 = 'x = add a b'
+        >>> l2 = 'x = add x c'
+        >>> env, prog = file2cfg_and_env([l0, l1, l2])
+        >>> interp(prog[0], env).get("x")
+        9
+    """
+    # TODO: Imlement this method.
+    env = line2env(lines[0])
+    insts = []
+
+    # Parsing das linhas
+    # Primeira passada cria as instruções
+    for i in range(1, len(lines)):
+        tokens = lines[i].split()
+        # Verificamos se estamos em um branch ou em uma instrução
+        if(tokens[1] == "="):
+            if(tokens[2] == "add"):
+                insts.append(Add(tokens[0], tokens[3], tokens[4]))
+            elif (tokens[2] == "mul"):
+                insts.append(Mul(tokens[0], tokens[3], tokens[4]))
+            elif (tokens[2] == "lth"):
+                insts.append(Lth(tokens[0], tokens[3], tokens[4]))
+            elif (tokens[2] == "geq"):
+                insts.append(Geq(tokens[0], tokens[3], tokens[4]))
+        else:
+            insts.append(Bt(tokens[1], None, None))
+        
+    # Adicionando a sentinela
+    insts.append(None)
+
+    # Costurando a lista de instruções
+    for i in range(len(insts)-1):
+        tokens = lines[i+1].split()
+        if(insts[i+1] is not None):
+                insts[i].add_next(insts[i+1])
+        
+        # Verificamos se a instrução é um branch
+        if(tokens[1] != "="):
+            if(insts[int(tokens[2])] is not None):
+                insts[i].add_true_next(insts[int(tokens[2])])
+        
+    # Removendo a sentinela
+    insts.pop()
+
+    return (env, insts)
